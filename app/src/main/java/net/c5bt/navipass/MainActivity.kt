0de9a6372f68aa69665333728769d7dc080d627a
ちょこばた <support@c5bt.net>
2018-07-08 22:46:25 +0900

    initial commit

---
+++ app/src/main/java/net/c5bt/navipass/MainActivity.kt
@@ -0,0 +1,12 @@
+package net.c5bt.navipass
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
